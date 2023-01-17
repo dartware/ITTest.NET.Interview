fcb32d7219cbc8f173a540d57b63b560a62fc98a
Dartware <dartware@mail.ru>
2023-01-17 09:47:26 +0300

    init

---
+++ AsyncLockDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncLockDemo;
+
+public class Program
+{
+
+	private static object lockObject = new();
+	private static int counter;
+
+	public static async Task Main(string[] args)
+	{
+		
+		for (int index = 0; index < 1_000_000; ++index)
+		{
+			Calculate();
+		}
+		
+		Console.WriteLine(counter);
+		Console.ReadLine();
+
+	}
+
+	private static async void Calculate()
+	{
+		Monitor.Enter(lockObject);
+		
+		await Task.Factory.StartNew(() =>
+		{
+			++counter;
+		});
+		
+		Monitor.Exit(lockObject);
+	}
+
+}
---
+++ UsingDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+string fileName = "File.txt";
+FileStream fileStream = File.OpenRead(fileName);
+
+fileStream = File.OpenRead(fileName);
+
+StreamReader reader = new StreamReader(fileStream, Encoding.UTF8);
+string text = await reader.ReadToEndAsync();
+
+Console.WriteLine(text);
+Console.ReadLine();
